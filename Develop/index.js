9d5aeca9bb0b1118b5de009bc1885e687295557f
YSL <ysl@YSLs-MacBook-Pro.local>
2024-04-06 19:06:24 -0400

    Brief description of the changes you've made

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+fs.writeFile(fileName, data, err => {
+        if(err) {
+        console.error(err);
+        return;
+        
+    }
+    console.log("README file generated successfully");
+});
+}
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+ inquirer.prompt(questions)
+  .then(answers => {
+    const markdown = generateMarkdown(answers);
+    writeToFile("README.md", markdown);
+      
+  })
+  .catch(error => {
+    console.error('Error occured during initialization', error);
+  })
+  }
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+  const licenseBadges = {
+    "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "Apache": "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    "GPL": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    "BSD": "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+    "None": ""
+  };
+  return licenseBadges[license] || '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  if (!license) {
+    return "";
+  }
+  const licenseLinks = {
+    "MIT": "https://opensource.org/licenses/MIT",
+    "Apache": "https://opensource.org/licenses/Apache-2.0",
+    "GPL": "https://www.gnu.org/licenses/gpl-3.0",
+    "BSD": "https://opensource.org/licenses/BSD-3-Clause",
+    "None": ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+return `## License
+
+This project is licensed under the ${license} License.](${renderLicenseLink(license)})`;
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
 `;
 }
 
